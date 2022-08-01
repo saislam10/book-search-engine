436f6bee300d0f7464c723e78f84fc8f1b16e049
Safwan Islam <saislam@davidson.edu>
2022-08-01 09:23:19 -0500

    added some necessary things to components signup and login page, the page runs fine after npm run develop but the signup form does not work yet

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -5,17 +5,13 @@ const { AuthenticationError } = require("apollo-server-express");
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
-
-            if (context.user) {
-                const userData = await User.findOne({})
-                    .select('-__v -password')
-                    .populate('books')
-
-                return userData;
-            }
-
-            throw new AuthenticationError('Not logged in')
-
+          if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id }).select(
+              "-__v -password"
+            );
+            return userData;
+          }
+          throw new AuthenticationError("Not logged in");
         },
 
     },
@@ -42,33 +38,28 @@ const resolvers = {
             return {token, user};
     
         },
-        saveBook: async (parent, args, context) => {
+        saveBook: async (parent, { input }, context) => {
             if (context.user) {
-
-                const updatedUser = await User.findByIdAndUpdate(
-                    { _id: context.user._id },
-                    { $addToSet: { savedBooks: args.input } },
-                    { new: true }
-                );
-
-                return updatedUser;
+              const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: input } },
+                { new: true, runValidators: true }
+              );
+              return updatedUser;
             }
-
-            throw new AuthenticationError('You need to be logged in!');
-        },
-        removeBook: async (parent, args, context) => {
+            throw new AuthenticationError("You need to be logged in!");
+          },
+          removeBook: async (parent, { bookId }, context) => {
             if (context.user) {
-                const updatedUser = await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $pull: { savedBooks: { bookId: args.bookId } } },
-                    { new: true }
-                );
-
-                return updatedUser;
+              const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: bookId } } },
+                { new: true }
+              );
+              return updatedUser;
             }
-
-            throw new AuthenticationError('You need to be logged in!');
-        }
+            throw new AuthenticationError("You need to be logged in!");
+          },
     }
 }
 
